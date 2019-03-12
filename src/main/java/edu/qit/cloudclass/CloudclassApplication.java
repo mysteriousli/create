3d3895ab39393712bb9799d35a71fbae915bbc42
Nic41981 <783415836@qq.com>
2019-03-12 17:38:50 +0800

    新工程

---
+++ src/main/java/edu/qit/cloudclass/CloudclassApplication.java
@@ -0,0 +1,13 @@
+package edu.qit.cloudclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudclassApplication.class, args);
+    }
+
+}
